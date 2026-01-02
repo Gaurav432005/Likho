212cc6b8f566ad3258f8d60c94f9aa53c171aafc
Gaurav432005 <gauravgrewal9206@gmail.com>
2026-01-02 17:49:17 +0530

    new logo

--- vite.config.js
+++ vite.config.js
@@ -11,31 +11,37 @@ export default defineConfig({
       registerType: 'autoUpdate',
 
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Likho',
-        short_name: 'Likho',
-        description: 'Share your daily stories, public ideas, and private diary entries.',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: '/logo.png', 
-            sizes: '192x192',
-            type: 'image/png'
-           
-          },
-          {
-            src: '/logo.png',
-            sizes: '512x512',
-            type: 'image/png'
-           
-          }
-        ]
-      }
+manifest: {
+  name: 'Likho',
+  short_name: 'Likho',
+  description: 'Share your daily stories, public ideas, and private diary entries.',
+theme_color: '#1c2541',
+background_color: '#1c2541',
+
+  display: 'standalone',
+  scope: '/',
+  start_url: '/',
+  orientation: 'portrait',
+  icons: [
+    {
+      src: '/logo.png',
+      sizes: '192x192',
+      type: 'image/png'
+    },
+    {
+      src: '/logo.png',
+      sizes: '512x512',
+      type: 'image/png'
+    },
+    {
+      src: '/logo.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable'
+    }
+  ]
+}
+
     })
   ],
 })
